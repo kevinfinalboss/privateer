4532ad4a770d439fa080f524198c376bc3822625
kevinfinalboss <kevinmg50@gmail.com>
2025-07-24 17:13:33 +0000

    feat: Add i18n package for internationalization

---
+++ internal/cli/i18n.go
@@ -0,0 +1,40 @@
+package cli
+
+import (
+	"github.com/kevinfinalboss/privateer/internal/config"
+	"github.com/kevinfinalboss/privateer/internal/logger"
+)
+
+var i18n *logger.Logger
+
+func initI18n() {
+	if cfgFile != "" {
+		if tempCfg, err := config.Load(cfgFile); err == nil && tempCfg != nil {
+			i18n = logger.NewWithConfig(tempCfg)
+			return
+		}
+	}
+
+	defaultCfg := &config.Config{
+		Settings: config.Settings{
+			Language: getLanguageFromFlags(),
+			LogLevel: "info",
+		},
+	}
+
+	i18n = logger.NewWithConfig(defaultCfg)
+}
+
+func getLanguageFromFlags() string {
+	if language != "" {
+		return language
+	}
+	return "pt-BR"
+}
+
+func getMessage(key string) string {
+	if i18n == nil {
+		initI18n()
+	}
+	return i18n.GetMessage(key)
+}
