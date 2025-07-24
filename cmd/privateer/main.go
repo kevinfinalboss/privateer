613c1c567f543707f996eeeb0802fe3f48ffc02e
kevinfinalboss <kevinmg50@gmail.com>
2025-07-24 17:11:42 +0000

    feat: Add version flag to display build information

--- cmd/privateer/main.go
+++ cmd/privateer/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/kevinfinalboss/privateer/internal/cli"
 )
 
+var (
+	Version   = "dev"
+	BuildTime = "unknown"
+	GitCommit = "unknown"
+)
+
 func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
+		fmt.Printf("Privateer %s\n", Version)
+		fmt.Printf("Build: %s\n", BuildTime)
+		fmt.Printf("Commit: %s\n", GitCommit)
+		return
+	}
+
 	if err := cli.Execute(); err != nil {
 		os.Exit(1)
 	}
