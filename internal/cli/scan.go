d12b64cc2d6b856a14fb336b7a9d6aee9c9a5a72
kevinfinalboss <kevinmg50@gmail.com>
2025-07-28 15:20:28 +0000

    feat: scan cluster images and validate against private registries

--- internal/cli/scan.go
+++ internal/cli/scan.go
@@ -1,7 +1,14 @@
 package cli
 
 import (
+	"context"
+	"fmt"
+	"sort"
+	"time"
+
 	"github.com/kevinfinalboss/privateer/internal/kubernetes"
+	"github.com/kevinfinalboss/privateer/internal/registry"
+	"github.com/kevinfinalboss/privateer/pkg/types"
 	"github.com/spf13/cobra"
 )
 
@@ -29,6 +36,17 @@ var scanGithubCmd = &cobra.Command{
 	},
 }
 
+type ScanResult struct {
+	PublicImages       []*types.ImageInfo
+	AvailableInPrivate map[string][]string
+	NotAvailableImages []*types.ImageInfo
+	RegistryStats      map[string]int
+	TotalScanned       int
+	TotalPublic        int
+	TotalAvailable     int
+	ScanDuration       time.Duration
+}
+
 func init() {
 	scanCmd.Short = getMessage("scan_short")
 	scanCmd.Long = getMessage("scan_long")
@@ -42,9 +60,40 @@ func init() {
 }
 
 func scanCluster() error {
+	startTime := time.Now()
+
 	client, err := kubernetes.NewClient(cfg, log)
 	if err != nil {
 		return err
+	}
+
+	registryManager := registry.NewManager(log)
+	for _, regConfig := range cfg.Registries {
+		if err := registryManager.AddRegistry(&regConfig); err != nil {
+			log.Warn("registry_add_failed").
+				Str("registry", regConfig.Name).
+				Err(err).
+				Send()
+			continue
+		}
+	}
+
+	if registryManager.GetRegistryCount() == 0 {
+		log.Warn("no_registries_configured").
+			Str("message", "Nenhum registry privado configurado").
+			Send()
+		return fmt.Errorf("nenhum registry privado configurado")
+	}
+
+	log.Info("registry_health_check_starting").
+		Int("registries", registryManager.GetRegistryCount()).
+		Send()
+
+	ctx := context.Background()
+	if err := registryManager.HealthCheck(ctx); err != nil {
+		log.Warn("registry_health_check_issues").
+			Err(err).
+			Send()
 	}
 
 	namespaces, err := client.GetNamespaces()
@@ -56,10 +105,16 @@ func scanCluster() error {
 	log.Info("scanning_cluster").
 		Int("namespace_count", len(namespaces)).
 		Strs("namespaces", namespaces).
+		Int("configured_registries", registryManager.GetRegistryCount()).
 		Send()
 
 	scanner := kubernetes.NewScanner(client, log, cfg)
-	totalPublicImages := 0
+	result := &ScanResult{
+		PublicImages:       make([]*types.ImageInfo, 0),
+		AvailableInPrivate: make(map[string][]string),
+		NotAvailableImages: make([]*types.ImageInfo, 0),
+		RegistryStats:      make(map[string]int),
+	}
 
 	for _, namespace := range namespaces {
 		publicImages, err := scanner.ScanNamespace(namespace)
@@ -71,28 +126,207 @@ func scanCluster() error {
 			continue
 		}
 
-		if len(publicImages) > 0 {
-			totalPublicImages += len(publicImages)
-
-			for _, image := range publicImages {
-				log.Info("public_image_found").
-					Str("image", image.Image).
-					Str("resource_type", image.ResourceType).
-					Str("resource_name", image.ResourceName).
-					Str("namespace", image.Namespace).
-					Str("container", image.Container).
-					Bool("is_init_container", image.IsInitContainer).
-					Send()
-			}
-		}
+		result.PublicImages = append(result.PublicImages, publicImages...)
 	}
+
+	result.TotalScanned = len(result.PublicImages)
+	result.TotalPublic = len(result.PublicImages)
+
+	log.Info("validating_images_in_registries").
+		Int("public_images", len(result.PublicImages)).
+		Send()
+
+	validatedMap, err := registryManager.ValidateImagesBatch(ctx, result.PublicImages, cfg)
+	if err != nil {
+		log.Warn("batch_validation_partial_failure").
+			Err(err).
+			Send()
+	}
+
+	for _, image := range result.PublicImages {
+		privateImage, registryName, err := registryManager.FindImageInRegistries(ctx, image, cfg)
+		if err != nil {
+			result.NotAvailableImages = append(result.NotAvailableImages, image)
+			log.Debug("image_not_found_in_private").
+				Str("public_image", image.Image).
+				Str("namespace", image.Namespace).
+				Str("resource", image.ResourceName).
+				Send()
+			continue
+		}
+
+		if result.AvailableInPrivate[image.Image] == nil {
+			result.AvailableInPrivate[image.Image] = make([]string, 0)
+		}
+		result.AvailableInPrivate[image.Image] = append(result.AvailableInPrivate[image.Image], fmt.Sprintf("%s (%s)", privateImage, registryName))
+		result.RegistryStats[registryName]++
+		result.TotalAvailable++
+	}
+
+	result.ScanDuration = time.Since(startTime)
+
+	printScanSummary(result, validatedMap)
+	printDetailedResults(result)
+	printRegistryStats(result)
+	printRecommendations(result)
 
 	log.Info("operation_completed").
 		Str("operation", "cluster_scan").
-		Int("total_public_images", totalPublicImages).
+		Int("total_scanned", result.TotalScanned).
+		Int("available_in_private", result.TotalAvailable).
+		Int("not_available", len(result.NotAvailableImages)).
+		Str("duration", result.ScanDuration.String()).
 		Send()
 
 	return nil
+}
+
+func printScanSummary(result *ScanResult, validatedMap map[string]string) {
+	log.Info("scan_summary").
+		Str("separator", "===========================================").
+		Send()
+
+	log.Info("scan_results_summary").
+		Int("total_images_scanned", result.TotalScanned).
+		Int("public_images_found", result.TotalPublic).
+		Int("available_in_private", result.TotalAvailable).
+		Int("not_available_in_private", len(result.NotAvailableImages)).
+		Int("validated_from_batch", len(validatedMap)).
+		Str("scan_duration", result.ScanDuration.String()).
+		Send()
+
+	if result.TotalPublic > 0 {
+		availabilityPercentage := float64(result.TotalAvailable) / float64(result.TotalPublic) * 100
+		log.Info("availability_percentage").
+			Float64("percentage", availabilityPercentage).
+			Send()
+	}
+}
+
+func printDetailedResults(result *ScanResult) {
+	if len(result.AvailableInPrivate) > 0 {
+		log.Info("images_available_in_private").
+			Str("separator", "-------------------------------------------").
+			Send()
+
+		sortedImages := make([]string, 0, len(result.AvailableInPrivate))
+		for image := range result.AvailableInPrivate {
+			sortedImages = append(sortedImages, image)
+		}
+		sort.Strings(sortedImages)
+
+		for _, image := range sortedImages {
+			registries := result.AvailableInPrivate[image]
+			log.Info("image_available").
+				Str("public_image", image).
+				Strs("private_locations", registries).
+				Int("registry_count", len(registries)).
+				Send()
+		}
+	}
+
+	if len(result.NotAvailableImages) > 0 {
+		log.Info("images_not_available_in_private").
+			Str("separator", "-------------------------------------------").
+			Send()
+
+		imageMap := make(map[string][]*types.ImageInfo)
+		for _, image := range result.NotAvailableImages {
+			imageMap[image.Image] = append(imageMap[image.Image], image)
+		}
+
+		sortedImages := make([]string, 0, len(imageMap))
+		for image := range imageMap {
+			sortedImages = append(sortedImages, image)
+		}
+		sort.Strings(sortedImages)
+
+		for _, imageName := range sortedImages {
+			images := imageMap[imageName]
+			usageCount := len(images)
+
+			namespaces := make([]string, 0)
+			resources := make([]string, 0)
+
+			for _, img := range images {
+				namespaces = append(namespaces, img.Namespace)
+				resources = append(resources, fmt.Sprintf("%s/%s", img.ResourceType, img.ResourceName))
+			}
+
+			log.Info("image_not_available").
+				Str("public_image", imageName).
+				Int("usage_count", usageCount).
+				Strs("namespaces", namespaces).
+				Strs("resources", resources).
+				Send()
+		}
+	}
+}
+
+func printRegistryStats(result *ScanResult) {
+	if len(result.RegistryStats) > 0 {
+		log.Info("registry_statistics").
+			Str("separator", "-------------------------------------------").
+			Send()
+
+		type registryStat struct {
+			name  string
+			count int
+		}
+
+		stats := make([]registryStat, 0, len(result.RegistryStats))
+		for name, count := range result.RegistryStats {
+			stats = append(stats, registryStat{name: name, count: count})
+		}
+
+		sort.Slice(stats, func(i, j int) bool {
+			return stats[i].count > stats[j].count
+		})
+
+		for _, stat := range stats {
+			percentage := float64(stat.count) / float64(result.TotalAvailable) * 100
+			log.Info("registry_stat").
+				Str("registry", stat.name).
+				Int("image_count", stat.count).
+				Float64("percentage", percentage).
+				Send()
+		}
+	}
+}
+
+func printRecommendations(result *ScanResult) {
+	log.Info("recommendations").
+		Str("separator", "===========================================").
+		Send()
+
+	if len(result.NotAvailableImages) > 0 {
+		log.Info("migration_recommendation").
+			Str("message", "Para migrar as imagens não disponíveis").
+			Str("command", "privateer migrate cluster --dry-run").
+			Send()
+
+		uniqueImages := make(map[string]bool)
+		for _, img := range result.NotAvailableImages {
+			uniqueImages[img.Image] = true
+		}
+
+		log.Info("migration_candidates").
+			Int("unique_images_to_migrate", len(uniqueImages)).
+			Send()
+	}
+
+	if result.TotalAvailable > 0 {
+		log.Info("gitops_recommendation").
+			Str("message", "Para atualizar repositórios com imagens disponíveis").
+			Str("command", "privateer migrate github --dry-run").
+			Send()
+	}
+
+	if len(result.RegistryStats) > 1 {
+		log.Info("consolidation_recommendation").
+			Str("message", "Considere consolidar imagens em registries menos utilizados").
+			Send()
+	}
 }
 
 func scanGithub() error {
